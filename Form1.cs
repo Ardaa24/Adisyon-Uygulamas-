9229b862e6a2580546fc2f3308a0d44b1eedea5f
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-04-21 20:25:52 +0300

    Yükleme Ekranı
    Uygulama açılırken yükleme ekranı.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coffeApp
+{
+    public partial class LoadScreen: Form
+    {
+
+        int i = 0;
+        public LoadScreen()
+        {
+            InitializeComponent();
+        }
+
+     
+
+     
+
+        private void LoadScreen_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i != 100)
+            {
+                i++;
+            }
+            else
+            {
+                timer1.Stop();
+                this.Hide();
+                Form2 form = new Form2();
+                form.Show();
+            }
+        }
+
+    }
+
+}
